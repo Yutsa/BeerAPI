4e053abf16691f37961ec16683dc26f41c7cafde
Édouard WILLISSECK <edouard.willisseck@gmail.com>
2018-10-21 13:09:45 +0200

    Documented the REST endpoints.

--- src/main/java/com/beerapi/BeerAPI/controller/BeerAPI.java
+++ src/main/java/com/beerapi/BeerAPI/controller/BeerAPI.java
@@ -8,16 +8,27 @@ import io.swagger.annotations.ApiResponses;
 
 public interface BeerAPI
 {
-  @ApiOperation(value = "Retrieve all the beers", nickname = "getAllBeers", tags={ "Beers" })
+  @ApiOperation(value = "Retrieve all the beers", nickname = "Retrieve all the beers", tags={ "Beers" })
+  @ApiResponses(value = {
+      @ApiResponse(code = 200, message = "Success") })
+  Iterable<Beer> getAllBeers();
+
+  @ApiOperation(value = "Retrieve a beer by its ID", nickname = "Retrieve a beer by ID", tags={ "Beers" })
   @ApiResponses(value = {
       @ApiResponse(code = 200, message = "Success"),
       @ApiResponse(code = 404, message = "Beer with id {id} not found") })
-  Iterable<Beer> getAllBeers();
-
   Beer getById(Long id);
 
+  @ApiOperation(value = "Deletes a beer by its ID", nickname = "Delete a beer by ID", tags={ "Beers" })
+  @ApiResponses(value = {
+      @ApiResponse(code = 200, message = "Success"),
+      @ApiResponse(code = 404, message = "Beer with id {id} not found") })
   void deleteById(Long id);
 
+  @ApiOperation(value = "Adds a beer", nickname = "Adds a beer", tags={ "Beers" })
+  @ApiResponses(value = {
+      @ApiResponse(code = 200, message = "Success"),
+      @ApiResponse(code = 404, message = "Beer with id {id} not found") })
   Beer addBeer(Beer newBeer);
 
   @ApiOperation(value = "Retrieve the beers that are like the one passed as parameter", nickname = "Similar Beers", tags={ "Beers"})
