ff3af40160f874a175e6ae9bb05e6a415d38eedc
Édouard WILLISSECK <edouard.willisseck@gmail.com>
2018-12-28 16:29:16 +0100

    Use an embedded database.

--- src/main/java/com/beerapi/beerapi/logging/ValueSanitizer.java
+++ src/main/java/com/beerapi/beerapi/logging/ValueSanitizer.java
@@ -3,7 +3,7 @@ package com.beerapi.beerapi.logging;
 public final class ValueSanitizer
 {
 
-  public static final String NEWLINE_PATTERN = "[\r\n]";
+  private static final String NEWLINE_PATTERN = "[\r\n]";
 
   private ValueSanitizer(){}
   public static String sanitizeInput(Object object) {
