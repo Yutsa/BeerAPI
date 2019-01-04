29766b5365ec71ed9fb87e9b94adf21255883823
Édouard WILLISSECK <edouard.willisseck@gmail.com>
2019-01-04 15:10:39 +0100

    More tests for the service layer.

--- src/main/java/com/beerapi/beerapi/services/BeerService.java
+++ src/main/java/com/beerapi/beerapi/services/BeerService.java
@@ -27,25 +27,6 @@ public class BeerService
     this.beerRepository = beerRepository;
   }
 
-  public Iterable<Beer> getSimilarBeer(final Long id) {
-    Optional<Beer> initialBeer = beerRepository.findById(id);
-
-    if (!initialBeer.isPresent())
-    {
-      String message = MessageFormat.format("Beer with id {0} not found", id);
-      LOGGER.warn(message);
-      throw new BeerNotFoundException(message);
-    }
-
-    double alcoholPercentage = initialBeer.get().getAlcoholPercentage();
-    Iterable<Beer> beerIterable = beerRepository.findBeerByAlcoholPercentage(alcoholPercentage);
-
-    return StreamSupport.stream(beerIterable.spliterator(), false)
-        .filter(b -> !b.getId().equals(id))
-        .collect(Collectors.toList());
-
-  }
-
   public Beer addBeer(final BeerResource newBeer) {
     Beer beer = new Beer(newBeer);
     return beerRepository.save(beer);
@@ -68,8 +49,7 @@ public class BeerService
     return beer.get();
   }
 
-  public Iterable<Beer> getAllBeers()
-  {
+  public Iterable<Beer> getAllBeers() {
     LOGGER.info("Getting all the beers.");
     return beerRepository.findAll();
   }
@@ -86,6 +66,25 @@ public class BeerService
     return Optional.ofNullable(query)
         .map(it -> it.replace("œ", "oe"))
         .orElse("");
+  }
+
+  public Iterable<Beer> getSimilarBeer(final Long id) {
+    Optional<Beer> initialBeer = beerRepository.findById(id);
+
+    if (!initialBeer.isPresent())
+    {
+      String message = MessageFormat.format("Beer with id {0} not found", id);
+      LOGGER.warn(message);
+      throw new BeerNotFoundException(message);
+    }
+
+    double alcoholPercentage = initialBeer.get().getAlcoholPercentage();
+    Iterable<Beer> beerIterable = beerRepository.findBeerByAlcoholPercentage(alcoholPercentage);
+
+    return StreamSupport.stream(beerIterable.spliterator(), false)
+        .filter(b -> !b.getId().equals(id))
+        .collect(Collectors.toList());
+
   }
 }
 
--- src/test/java/com/beerapi/beerapi/BeerServiceTest.java
+++ src/test/java/com/beerapi/beerapi/BeerServiceTest.java
@@ -1,12 +1,17 @@
 package com.beerapi.beerapi;
 
+import com.beerapi.beerapi.exception.BeerNotFoundException;
+import com.beerapi.beerapi.model.entities.Beer;
 import com.beerapi.beerapi.repository.BeerRepository;
 import com.beerapi.beerapi.services.BeerService;
 
 import org.junit.Test;
 
+import java.util.Optional;
+
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class BeerServiceTest
 {
@@ -26,5 +31,26 @@ public class BeerServiceTest
 
     beerService.searchBeer(null);
     verify(mockBeerRepository).findBeerByNameIgnoreCaseContaining("");
+  }
+  
+  @Test
+  public void similarBeer_UseCorrectPercentage() {
+    BeerRepository mockBeerRepository = mock(BeerRepository.class);
+    when(mockBeerRepository.findById(3L))
+        .thenReturn(Optional.of(new Beer(12, "TestBeer")));
+
+    BeerService beerService = new BeerService(mockBeerRepository);
+
+    beerService.getSimilarBeer(3L);
+    verify(mockBeerRepository).findBeerByAlcoholPercentage(12);
+  }
+
+  @Test(expected = BeerNotFoundException.class)
+  public void similarBeer_BeerNotFound() {
+    BeerRepository mockBeerRepository = mock(BeerRepository.class);
+    when(mockBeerRepository.findById(3L)).thenReturn(Optional.empty());
+    BeerService beerService = new BeerService(mockBeerRepository);
+
+    beerService.getSimilarBeer(3L);
   }
 }
